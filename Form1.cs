0bccfe30fa5a181b56f054ff730f200df3cdecc6
Buz Maltby <buzmaltby@gmail.com>
2023-12-30 17:24:33 -0500

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DesktopCalculator
+{
+    public partial class Form1 : Form
+    {
+        OPS C = new OPS();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtOutPut.Focus();
+            C.OP1 = 0; 
+            C.OP2 = 0;
+            C.OP = "";
+            C.State = 0; 
+            
+        }
+   
+        private void button7_Click(object sender, EventArgs e)
+        {
+            txtOutPut.Text = Calc.NumberButtonPush(txtOutPut.Text, "7",ref C);
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            txtOutPut.Text = Calc.NumberButtonPush(txtOutPut.Text, "8",ref C);
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            txtOutPut.Text = Calc.NumberButtonPush(txtOutPut.Text, "9",ref C);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            txtOutPut.Text = Calc.NumberButtonPush(txtOutPut.Text, "4",ref C);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            txtOutPut.Text = Calc.NumberButtonPush(txtOutPut.Text, "5",ref C );
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            txtOutPut.Text = Calc.NumberButtonPush(txtOutPut.Text, "6",ref C);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtOutPut.Text = Calc.NumberButtonPush(txtOutPut.Text, "1",ref C);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            txtOutPut.Text = Calc.NumberButtonPush(txtOutPut.Text, "2",ref C);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            txtOutPut.Text = Calc.NumberButtonPush(txtOutPut.Text, "3",ref C);
+        }
+
+        private void button0_Click(object sender, EventArgs e)
+        {
+            txtOutPut.Text = Calc.NumberButtonPush(txtOutPut.Text, "0",ref C);
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            //already a decimal point in operand? if so, ignore
+            if (txtOutPut.Text.Contains(".")) return;
+            //put one in if none there yet
+            txtOutPut.Text = Calc.NumberButtonPush(txtOutPut.Text, ".",ref C);
+        }
+
+        private void btnMinus_Click(object sender, EventArgs e)
+        {
+            if (C.State == 1) { hist0.Text = txtOutPut.Text + "-"; }
+            //record OP1 and OP in history
+            if (C.State == 3) 
+            { //record full equation in history
+                hist0.Text = hist0.Text + txtOutPut.Text; 
+            }
+            txtOutPut.Text = Calc.NegButtonPush(txtOutPut.Text,ref C);
+        }
+
+        private void btnEqual_Click(object sender, EventArgs e)
+        {
+            txtOutPut.Text = Calc.Equals(txtOutPut.Text, ref C);
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            txtOutPut.Text = "";
+
+        }
+
+        private void btnPlus_Click(object sender, EventArgs e)
+        {
+            if (C.State == 2 | C.State == 3)
+            { 
+                txtOutPut.Text = Calc.PlusButtonPush(txtOutPut.Text, ref C);
+                hist0.Text = txtOutPut.Text + "+";
+            }
+            
+        }
+
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace DesktopCalculator
+{
+    internal static class Program
+    {
+
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+
+
+        static void Main()
+        {
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+
+
+        }
+    }
+    public struct OPS
+    {
+        public double OP1;//operand 1
+        public double OP2;//operand 2
+        public string OP;//operator 
+        //state 0 waiting for first character of OP1
+        public int State;//getting OP1
+      
+        
+
+    }
+
+
+    public class Calc
+    {
+
+        public static string NumberButtonPush(string txt, string newnum, ref OPS C)
+        {
+            if (C.State == 0)//state 0
+               { C.State = 1; return newnum; }//first digit of OP1
+
+            if (C.State == 2) 
+            { C.State = 3;  return newnum; }//first digit of OP2
+            //move from state 2 to state 3
+            return txt + newnum;
+        }
+        public static string PlusButtonPush(string txt,ref OPS C)
+        {
+            string result;
+            C.OP = "+"; //store operator
+            if (C.State == 3) //stringing together operations
+            {
+                if (!double.TryParse(txt, out C.OP2))//convert to double
+                    return ("Error. Not a number");
+                result = DoCalc(ref C);
+                return result;
+            }
+            if (!double.TryParse(txt, out C.OP1))//convert to double
+                return ("Error. Not a number");
+          
+            result = ""; //return blank to await operand 2
+            return result;
+
+        }
+        public static string NegButtonPush(string txt,ref OPS C)
+        {
+            string result;
+            if (C.State == 0) //state 0
+            {
+                //C.OP1 = 0;
+                return ("-"); //no numbers yet, making a negative number
+            }
+            else if (C.State == 1)//state 1
+            {
+                if (!double.TryParse(txt, out C.OP1))//convert to double
+                    return ("Error. Not a number");
+                //store current entry 
+                C.OP = "-";// doing subtraction. store operand 1 and operator
+                C.State  = 2;
+                //move to state 2
+                return ("");//clear input for second operand
+           }
+            else //only case left is going to do another subtraction
+            {//got to think about this some more.
+                //T
+                if (!double.TryParse(txt, out C.OP2))//convert to double
+                    return ("Error. Not a number");
+                //**** review the following!!!
+                result = DoCalc(ref C); //do the calulation
+                C.OP = "-";//store the operand
+                           // if TryParse()
+                if (!double.TryParse(txt, out C.OP1))//convert to double
+                    return ("Error. Not a number");
+                //go to state 2 waiting for another subtrahend
+                C.State = 2;
+                return result;
+            }
+        }
+        public static string DoCalc(ref OPS C)
+
+        {
+            double CalcResult;
+            if (C.OP == "+")//add
+                CalcResult = C.OP1 + C.OP2;
+            else if (C.OP == "-")//subtract
+                CalcResult = C.OP1 - C.OP2;
+            else if (C.OP == "*")//multiply
+                CalcResult = C.OP1 * C.OP2;
+            else //if (C.OP == "/")//divide
+                try
+                { CalcResult = C.OP1 / C.OP2; }
+                catch //oops, div by 0
+                {
+                    CalcResult = 0;
+                    //make em start over at state 0
+                    C.OP = "";
+                    C.OP2 = 0;
+                    C.OP1 = 0;
+                    C.State = 0;
+                    
+                    return "div 0 ERROR";
+                }
+            C.State = 2;//go to state 1
+ 
+            C.OP1 = CalcResult;//put result in OP1
+            C.OP2 = 0;
+            C.OP = "";
+            return CalcResult.ToString();
+
+
+        }
+        public static string Equals(string txt, ref OPS C)
+        {
+            string result;
+            if (!double.TryParse(txt, out C.OP2))//convert to double
+                return ("Error. Not a number");
+            result = DoCalc(ref C); //string for the return
+                                //first reset C values
+            if (!double.TryParse(txt, out C.OP1))
+                return ("Error");
+            C.OP = "";
+            C.OP2 = 0;
+            return result;
+
+
+
+        }
+        public static bool ClearOPS(ref OPS C )
+        {
+            C.OP1 = 0;
+            C.OP2 = 0;
+            C.OP = "";
+            return true;
+        }
+
+
+    }
+}
